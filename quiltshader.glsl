0cd0a1c67840b2d716d06c31845320775e23bba7
Yann Vernier <yann-git@vernier.se>
2018-12-31 14:32:25 +0100

    mpv shader with corrected formulae
    The subpixel offset needed to be scaled by pitch.
    The tile order turned out to be different directions
    vertically and horisontally.

--- quiltshader.glsl
+++ quiltshader.glsl
@@ -1,6 +1,7 @@
 // mpv glsl shader hook for looking glass
 // Usage sample:
 //  mpv --screen=1 --fs-screen=1 --fs --glsl-shader=quiltshader.glsl --no-keepaspect *.mp4
+// Decent sample frame: Holo Reality at 26 seconds, -ss 26 Holo*.mp4
 
 //!HOOK MAINPRESUB
 //!DESC Looking Glass Quilt renderer
@@ -11,26 +12,22 @@
 // TODO: Fill these in from HID calibration data.
 const float tilt = -0.12039111681976107; //{tilt};
 const float pitch = 370.66407267416486; //{pitch};
-const float center = 0.13695651292800903; //{center};
-const float subp = 1.0 / (3*2560); //{subp};
+const float center = 0.5 + 0.13695651292800903; //{center};
+const float subp = 1.0 / (3*2560) * pitch; //{subp};
 
-// FIXME: I have two layers of errors in this rendering.
-// First it has severe colour shifts. 
-// Second it has discontinuities suggesting the tile mapping is badly ordered.
-// Thirdly, not all the streams are 5x9 quilts.
+// not all the streams are 5x9 quilts.
 // For instance Baby* is 4x8
 
-const float quilt_width = 5;
-const float quilt_height = 9;
-const float quilt_stride = 1.0 / quilt_height;
-const vec2 quilt_scale = vec2 (1.0/quilt_width, 1.0/quilt_height);
+const vec2 tiles = vec2(5,9);
 
 vec2 quilt_map(vec2 pos, float a) {
-  vec2 tile = vec2(0,0);
-  a = fract(a)*quilt_width;
-  tile.y = floor(a) * quilt_scale.y;
-  tile.x = floor(mod(a*quilt_height, quilt_width)) * quilt_scale.x;
-  return fma(pos, quilt_scale, tile);
+  // Y major positive direction, X minor negative direction
+  vec2 tile = vec2(tiles.x-1,0), dir=vec2(-1,1);
+  a = fract(a)*tiles.y;
+  tile.y += dir.y*floor(a);
+  a = fract(a)*tiles.x;
+  tile.x += dir.x*floor(a);
+  return (tile+pos)/tiles;
 }
 
 vec4 hook() {
