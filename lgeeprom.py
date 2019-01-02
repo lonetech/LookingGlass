796a2c0184fd643cc6e255c4dc3cc46305e97276
Yann Vernier <yann-git@vernier.se>
2019-01-02 16:20:52 +0100

    Updated lgeeprom to use hidapi rather than hid
    Essentially the same thing, but it seems more reliable
    at least when combined with a hidraw using hidapi.
    Also added button reading.

--- lgeeprom.py
+++ lgeeprom.py
@@ -2,44 +2,56 @@ from pprint import pprint
 import json
 import struct
 
-import hid
+# Note: Use libhidapi-hidraw, i.e. hidapi with hidraw support,
+# or the joystick device will be gone when execution finishes.
+import hidapi
+
+pagesize = 64
+
 
 def rp(dev, addr=0, size=64):
-    send = bytearray(struct.pack('>BBH64x', 0, 0, addr))
-    #print repr(send)
-    #print len(send)
-    dev.send_feature_report(send)
-    r = bytearray(dev.read(64, 1000))
-    r += bytearray(dev.read(3+1, 1000))
-    #print "read: ", len(r), r
-    assert r[:4] == send[:4]
+    send = bytearray(struct.pack('>BH64x', 0, addr))
+    dev.send_feature_report(send, b'\0')
+    r = bytearray(dev.read(1+1+2+64, 1000))
+    #print("read: ", len(r), r)
+    # First byte holds button bitmask
+    # second byte is command for EEPROM management (0=read)
+    # third and fourth are EEPROM page address
+    # Verify 1:4 so we are reading the correct data
+    assert r[1:4] == send[:3]
     return r[4:4+size]
 
+
 def loadconfig(dev):
-    jsonlen=struct.unpack('>I',rp(dev, 0, 4))[0] + 4
+    jsonlen = struct.unpack('>I', rp(dev, 0, 4))[0] + 4
     assert jsonlen != 0xffffffff
     #print jsonlen
-    json=bytearray()
-    while len(json)<jsonlen:
-        page=len(json)//64
-        l=min(64,jsonlen-64*page)
+    json = bytearray()
+    while len(json) < jsonlen:
+        page = len(json)//64
+        l = min(64, jsonlen-64*page)
         json[64*page:] = rp(dev, page, l)
         #print json
     return json[4:]
 
+
 def read_eeprom(devinfo):
-    dev = hid.device()
-    dev.open_path(devinfo['path'])
-    dev.set_nonblocking(1)
-    #r=True
-    #while r:
-    #    r = bytearray(dev.read(64+3+1, 1000))
-    #    #print r
+    dev = hidapi.Device(devinfo)
     cfg = json.loads(loadconfig(dev).decode('ascii'))
     pprint(cfg)
-    return cfg
+    # TODO: Use calibration data. Sample code in lgdisplaytest.py
+    return (dev, cfg)
 
-for dev in hid.enumerate(vendor_id=0x04d8, product_id=0xef7e):
-    if dev['product_string'] == u'HoloPlay':
-        read_eeprom(dev)
 
+for dev in hidapi.enumerate(vendor_id=0x04d8, product_id=0xef7e):
+    if dev.product_string == u'HoloPlay':
+        hiddev, cfg = read_eeprom(dev)
+        while True:
+            # Keep reading the button bitmask
+            r = hiddev.read(1)
+            if r:
+                byte = r[0]
+                # Python 2 compatibility
+                if isinstance(byte, str):
+                    byte = ord(byte)
+                print('{:04b}'.format(byte))
