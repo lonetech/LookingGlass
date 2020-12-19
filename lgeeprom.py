db90c47dabca2315baa67841cd410c19ec175008
Yann Vernier <yann-git@vernier.se>
2020-12-19 19:48:58 +0100

    More robust HID reading.
    Not sure which backend I have today, but I can get single reports split
    in multiple reads. Try to sync up properly.

--- lgeeprom.py
+++ lgeeprom.py
@@ -12,7 +12,11 @@ pagesize = 64
 def rp(dev, addr=0, size=64):
     send = bytearray(struct.pack('>BH64x', 0, addr))
     dev.send_feature_report(send, b'\0')
-    r = bytearray(dev.read(1+1+2+64, 1000))
+    r = bytearray(dev.read(1+1+2+64, timeout_ms=1000))
+    while r[1:4] != send[:3]:
+        r = bytearray(dev.read(1+1+2+64, timeout_ms=1000))
+    if len(r) < 1+1+2+64:
+        r += bytearray(dev.read(1+1+2+64-len(r), timeout_ms=10))
     #print("read: ", len(r), r)
     # First byte holds button bitmask
     # second byte is command for EEPROM management (0=read)
@@ -23,6 +27,10 @@ def rp(dev, addr=0, size=64):
 
 
 def loadconfig(dev):
+    # Flush input first
+    more=True
+    while more:
+        more = dev.read(68, blocking=False, timeout_ms=100)
     jsonlen = struct.unpack('>I', rp(dev, 0, 4))[0] + 4
     assert jsonlen != 0xffffffff
     #print jsonlen
@@ -48,10 +56,13 @@ for dev in hidapi.enumerate(vendor_id=0x04d8, product_id=0xef7e):
         hiddev, cfg = read_eeprom(dev)
         while True:
             # Keep reading the button bitmask
-            r = hiddev.read(1)
+            r = hiddev.read(68, blocking=True)
+            if len(r)<68:
+                r += hiddev.read(68-len(r), blocking=False)
             if r:
                 byte = r[0]
                 # Python 2 compatibility
                 if isinstance(byte, str):
                     byte = ord(byte)
                 print('{:04b}'.format(byte))
+
